0a728f64dd66a06cab460e609c9e03b0c8c6be16
Belen Curcio <curciobelen@gmail.com>
2016-11-24 16:35:45 -0300

    Add all routes

--- examples/minimal-react/server.js
+++ examples/minimal-react/server.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.static(path.resolve('build')))
 
-app.get('/', (req, res) => res.sendFile('index.html'))
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')))
 
 app.listen(3000)

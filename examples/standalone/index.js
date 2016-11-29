1e85f21218e66f3e2e9359f5e818620407ada4d4
Belen Curcio <curciobelen@gmail.com>
2016-11-29 08:55:11 -0300

    Standalone example

---
+++ examples/standalone/index.js
@@ -0,0 +1,34 @@
+/*
+
+   JSX Transpiled with https://jsx-live.now.sh/
+
+  <div>
+    <h1>Hola!</h1>
+    <h2>Hoy es {new Date().toLocaleTimeString()}.</h2>
+  </div>
+
+ */
+
+"use strict";
+
+var element = React.createElement(
+ "div",
+ null,
+ React.createElement(
+   "h1",
+   null,
+   "Hola!"
+ ),
+ React.createElement(
+   "h2",
+   null,
+   "Hoy es ",
+   new Date().toLocaleTimeString(),
+   "."
+ )
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);

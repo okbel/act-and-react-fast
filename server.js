9ae4a28ea2f228fe0dfc9109dc7006f9f532b637
Belen Curcio <curciobelen@gmail.com>
2016-12-02 01:32:52 -0300

    Server

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.resolve('/src')))
-app.get('*', (req, res) => {
+app.use(express.static(path.resolve('node_modules/react-dom/dist/')))
+app.use(express.static(path.resolve('node_modules/react/dist/')))
+app.use(express.static(path.resolve('node_modules/babel-standalone')))
+
+app.use(express.static(path.resolve('src')))
+app.use('/examples', express.static(path.resolve('examples')))
+
+app.get('/', (req, res) => {
   res.sendFile(path.resolve('src','index.html'))
 })
 
